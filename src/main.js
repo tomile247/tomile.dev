661d9b7de4448d15516223f66bbf6f35b76f289b
Tomi Levkus <tomi.levkus@gmail.com>
2025-03-25 21:02:44 +0100

    DESIGN: Implement header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navTrigger = document.querySelector('.nav-trigger');
+
+document.addEventListener('scroll', event => {
+    if(scrollY >= 100) {
+        header.setAttribute('scrolled', '');
+    } else {
+        header.removeAttribute('scrolled');
+    }
+})
+
+navTrigger.addEventListener('click', event => {
+    nav.toggleAttribute('opened');
+    if(nav.hasAttribute('opened')) {
+        navTrigger.setAttribute('src', '/assets/icons/close.svg');
+    } else {
+        navTrigger.setAttribute('src', '/assets/icons/menu.svg');
+    }
+})

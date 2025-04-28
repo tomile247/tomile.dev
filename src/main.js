9e2a112a752679d220f58099fd716253b328083c
Tomi Levkus <tomi.levkus@gmail.com>
2025-04-28 22:35:30 +0200

    DESIGN: Theme switcher implementation

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 const navTrigger = document.querySelector('.nav-trigger');
 const scrollToTop = document.querySelector('.js-scroll-to-top');
+const darkMode = document.querySelector('.js-dark-mode');
+const lightMode = document.querySelector('.js-light-mode');
+
+document.addEventListener('DOMContentLoaded', event => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.add('light-theme');
+    }
+});
 
 document.addEventListener('scroll', event => {
 
@@ -30,3 +41,16 @@ navTrigger.addEventListener('click', event => {
         navTrigger.innerHTML = `<use href="/icons.svg#menu"></use>`;
     }
 })
+
+darkMode.addEventListener('click', event => {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+
+});
+
+lightMode.addEventListener('click', event => {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light');
+});
